936dd69e8984e5b75b97af9cda92aaaad0959a67
Hannu Eloranta <hannu.eloranta@f-secure.com>
2013-04-27 07:49:07 +0300

    using table

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,33 +1,19 @@
 function Controller($scope){
-  $scope.number = [
-  {"value": 0,  "color":"white", "prime":0},
-  {"value": 1,  "color":"white", "prime":0},
-  {"value": 2,  "color":"gray", "prime":-1},
-  {"value": 3,  "color":"gray", "prime":-1},
-  {"value": 4,  "color":"gray", "prime":-1},
-  {"value": 5,  "color":"gray", "prime":-1},
-  {"value": 6,  "color":"gray", "prime":-1},
-  {"value": 7,  "color":"gray", "prime":-1},
-  {"value": 8,  "color":"gray", "prime":-1},
-  {"value": 9,  "color":"gray", "prime":-1},
-  {"value": 10, "color":"gray", "prime":-1},
-  {"value": 11, "color":"gray", "prime":-1},
-  {"value": 12, "color":"gray", "prime":-1},
-  {"value": 13, "color":"gray", "prime":-1},
-  {"value": 14, "color":"gray", "prime":-1},
-  {"value": 15, "color":"gray", "prime":-1},
-  {"value": 16, "color":"gray", "prime":-1},
-  {"value": 17, "color":"gray", "prime":-1},
-  {"value": 18, "color":"gray", "prime":-1},
-  {"value": 19, "color":"gray", "prime":-1},
-  {"value": 20, "color":"gray", "prime":-1}
-  ];
+
+  $scope.rows = [0, 1, 2, 3];
+  $scope.cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  $scope.number = [];
+  $scope.number.push({"value": 1, "color":"white", "prime":0});
+  for (var i = 2; i <= 40; ++i){
+    $scope.number.push({"value": i, "color":"gray", "prime":-1});
+  }
 
   $scope.n = $scope.prime = 2;
   
   var timer = setInterval(function(){
     if ($scope.n == -1){
-      for (var i = 2; i < $scope.number.length; ++i){
+      for (var i = 1; i < $scope.number.length; ++i){
         $scope.number[i].prime = -1;
         $scope.number[i].color = "gray";
       }
@@ -36,19 +22,19 @@ function Controller($scope){
       return;
     }
     if ($scope.n == $scope.prime){
-      $scope.number[$scope.n].prime = 1;
-      $scope.number[$scope.n].color = "red";
+      $scope.number[$scope.n-1].prime = 1;
+      $scope.number[$scope.n-1].color = "red";
       $scope.n += $scope.prime;
-      if ($scope.n >= $scope.number.length){
+      if ($scope.n  > $scope.number[$scope.number.length-1].value){
         $scope.n = $scope.prime = findNextPrime($scope);
       }
       $scope.$apply();
       return;
     }
-    $scope.number[$scope.n].prime = 0;
-    $scope.number[$scope.n].color = "cyan";
+    $scope.number[$scope.n-1].prime = 0;
+    $scope.number[$scope.n-1].color = "cyan";
     $scope.n += $scope.prime;
-    if ($scope.n >= $scope.number.length){
+    if ($scope.n > $scope.number[$scope.number.length-1].value){
       $scope.n = $scope.prime = findNextPrime($scope);
     }
     $scope.$apply();
@@ -57,7 +43,7 @@ function Controller($scope){
   function findNextPrime(scope){
     for (var i = 0; i < scope.number.length; ++i){
       if (scope.number[i].prime == -1){
-        return i;
+        return scope.number[i].value;
       }
     }
     return -1;
