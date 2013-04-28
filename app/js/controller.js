87fb95fbd0adbe70ad3d2071cda23868155f53b2
Hannu Eloranta <hannu.eloranta@f-secure.com>
2013-04-28 06:39:42 +0300

    refactor

--- app/js/controller.js
+++ app/js/controller.js
@@ -11,40 +11,43 @@ function Controller($scope){
   $scope.n = $scope.prime = 2;
   
   var timer = setInterval(function(){
-    if ($scope.n == -1){
-      for (var i = 1; i < $scope.number.length; ++i){
-        $scope.number[i].prime = -1;
-        $scope.number[i].color = "gray";
-      }
-      $scope.n = $scope.prime = 2;
-      $scope.$apply();
-      return;
-    }
-    if ($scope.n == $scope.prime){
-      $scope.number[$scope.n-1].prime = 1;
-      $scope.number[$scope.n-1].color = "red";
-      $scope.n += $scope.prime;
-      if ($scope.n  > $scope.number[$scope.number.length-1].value){
-        $scope.n = $scope.prime = findNextPrime($scope);
-      }
-      $scope.$apply();
-      return;
-    }
-    $scope.number[$scope.n-1].prime = 0;
-    $scope.number[$scope.n-1].color = "cyan";
-    $scope.n += $scope.prime;
-    if ($scope.n > $scope.number[$scope.number.length-1].value){
-      $scope.n = $scope.prime = findNextPrime($scope);
-    }
-    $scope.$apply();
+    if ($scope.n == -1)
+      reset();
+    else if ($scope.n == $scope.prime)
+      handleElement(true, "red");
+    else
+      handleElement(false, "cyan");
   }, 100);
 
-  function findNextPrime(scope){
-    for (var i = 0; i < scope.number.length; ++i){
-      if (scope.number[i].prime == -1){
-        return scope.number[i].value;
+  function reset(){
+    for (var i = 1; i < $scope.number.length; ++i){
+      $scope.number[i].prime = -1;
+      $scope.number[i].color = "gray";
+    }
+    $scope.n = $scope.prime = 2;
+    $scope.$apply();
+  }
+
+  function handleElement(prime, color){
+    $scope.number[$scope.n-1].prime = prime ? 1 : 0;
+    $scope.number[$scope.n-1].color = color;
+    $scope.n += $scope.prime;
+    if ($scope.n > $scope.number[$scope.number.length-1].value){
+      $scope.n = $scope.prime = findNextPrime();
+    }
+    $scope.$apply();
+  }
+  
+  function findNextPrime(){
+    for (var i = 0; i < $scope.number.length; ++i){
+      if ($scope.number[i].prime == -1){
+        return $scope.number[i].value;
       }
     }
     return -1;
   }
+
+
 }
+
+
